21bcafb1188238b0ab8fddf6022154e921bf7b5f
I Woithe <iwoithe@just42.net>
2021-12-05 18:21:13 +1030

    Make an action controller instance - Adds it to QML - Add a small test example of an action - The example doesn't work yet

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,42 @@
 #include <kddockwidgets/private/DockRegistry_p.h>
 #include <kddockwidgets/FrameworkWidgetFactory.h>
 
+#include <QAction>
+#include <QApplication>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
 #include <QQmlContext>
 #include <QString>
+#include <QDebug>
 
 #include "framework/actions/actioncontroller.h"
 
 #include "ui/register/register.h"
 
 
+void printHello() {
+    qDebug() << "Hello";
+}
+
+
 int main(int argc, char *argv[]) {
+    // QApplication app(argc, argv);
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     KDDockWidgets::Config::self().setQmlEngine(&engine);
+    
+    Pretzel::Framework::ActionController *actionController = new Pretzel::Framework::ActionController();
+    
+    QString printHelloName("hello");
+    QAction printHelloQAction(printHelloName);
+    // connect(printHelloQAction, &QAction::triggered, this, &printHello);
+    Pretzel::Framework::Action printHelloAction = {printHelloName, &printHelloQAction};
+    actionController->addAction(&printHelloAction);
 
     QString actionControllerName("ActionController");
-    Pretzel::Framework::ActionController *actionController = new Pretzel::Framework::ActionController();
-    Pretzel::Ui::registerObject(actionControllerName, actionController);
+    Pretzel::Ui::registerObject(engine.rootContext(), actionControllerName, actionController);
 
     const QUrl url("qrc:/ui/main.qml");
     engine.load(url);
